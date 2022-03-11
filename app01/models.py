bb7657a62c68c6bb0b2ab78c5d9f27b44acf8d27
Jim <chengxuyuan9527@gmail.com>
2022-03-12 00:30:04 +1100

    Add ORM for managing MySQl

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class UserInfo(models.Model):
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    age = models.IntegerField()
+
+
+class UserDepartment(models.Model):
+    section = models.CharField(max_length=50)
+
+
+# class Title(models.Model):
+#     title_name = models.CharField(max_length=30)
+#     title_number = models.IntegerField()
